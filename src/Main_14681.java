e1d5751026c339e6a2c0eb890d68e688775e4407
minsu <danbikma@naver.com>
2022-02-20 04:36:54 +0900

    baekjoon 14681

---
+++ src/Main_14681.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main_14681 {
+
+	public static void main(String[] args) {
+		
+		int x, y;
+		Scanner scanner = new Scanner(System.in);
+		x = scanner.nextInt();
+		y = scanner.nextInt();
+		
+		if(((x>=1)&&(x<=1000)) && ((y>=1)&&(y<=1000))) {
+			System.out.println("1");
+		} else if(((x>=1)&&(x<=1000)) && ((y>=-1000)&&(y<=-1))) {
+			System.out.println("4");
+		} else if(((x>=-1000)&&(x<=-1)) && ((y>=-1000)&&(y<=-1))) {
+			System.out.println("3");
+		} else if(((x>=-1000)&&(x<=-1)) && ((y>=1)&&(y<=1000))) {
+			System.out.println("2");
+		}
+		
+	}
+
+}
