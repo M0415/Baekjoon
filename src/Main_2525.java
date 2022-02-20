aadbdecbcc0927451887fbd476536f577eb3841e
minsu <danbikma@naver.com>
2022-02-20 19:09:58 +0900

    baekjoon 2525

---
+++ src/Main_2525.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main_2525 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int A = scanner.nextInt();
+		int B = scanner.nextInt();
+		int C = scanner.nextInt();
+		
+		B = B+C;
+		while(B>=60) {
+			if(B>=60) {
+				B = B-60;
+				A++;
+			}
+			if(A>=24) {
+				A = A-24;
+			}
+		}
+		System.out.printf("%d %d",A,B);
+		
+
+	}
+
+}
