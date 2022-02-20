c4c24505bdd1ead53766d80cee64e438bcd5d8fd
minsu <danbikma@naver.com>
2022-02-20 16:51:14 +0900

    baekjoon 10951

---
+++ src/Main_10951.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main_10951 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		while(scanner.hasNextInt()) {
+			int A = scanner.nextInt();
+			int B = scanner.nextInt();
+			int sum = A+B;
+			
+			System.out.println(sum);
+		}
+
+	}
+
+}
