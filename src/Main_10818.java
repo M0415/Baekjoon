59dfbebd363751692b4935deb91a6d56906dca15
minsu <danbikma@naver.com>
2022-02-21 22:30:20 +0900

    baekjoons 10818

---
+++ src/Main_10818.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main_10818 {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		int N = scanner.nextInt();
+		int[] arr = new int[N];
+		int min=1000000;
+		int max=-1000000;
+		
+		for(int i=0; i<arr.length; i++ ) {
+			arr[i] = scanner.nextInt();
+		}
+		
+		for(int i=0; i<arr.length; i++) {
+			if(max<arr[i]) {
+				max = arr[i];			
+			} 
+			if (min>arr[i]) {
+				min = arr[i];
+			}
+		}
+		System.out.printf("%d %d",min,max);
+
+	}
+
+}
