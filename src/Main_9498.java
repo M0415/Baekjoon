d2bd37e12add7427cb57d60c3c1ca35766bc6058
minsu <danbikma@naver.com>
2022-02-22 21:01:04 +0900

    시험 성적

--- src/Main_9498.java
+++ src/Main_9498.java
@@ -5,7 +5,7 @@ public class Main_9498 {
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
-		int goal = scanner.nextInt();   //점수 입력
+		int goal = scanner.nextInt();   //점수 입력.
 		
 		if(goal>=90 && goal<=100) {
 			System.out.println("A");
