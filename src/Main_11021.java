00ae14cb7c2db8866603cb0dad7e1e0781df6db6
minsu <danbikma@naver.com>
2022-02-22 20:35:41 +0900

    baekjoon 11021

---
+++ src/Main_11021.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main_11021 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int T = scanner.nextInt();
+		int []sum = new int[T];
+		
+		for(int i=0; i<T; i++) {
+			int A = scanner.nextInt();
+			int B = scanner.nextInt();
+			sum[i] = A+B;
+		}
+		for(int i=0; i<T; i++) {
+		System.out.printf("Case #%d: %d\n",i+1,sum[i]);
+		}
+
+	}
+
+}
