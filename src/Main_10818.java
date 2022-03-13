ad72138e954fe01de2df10ea2c555c9041de57c1
minsu <danbikma@naver.com>
2022-03-13 22:03:24 +0900

    최대 최소

--- src/Main_10818.java
+++ src/Main_10818.java
@@ -6,8 +6,8 @@ public class Main_10818 {
 		Scanner scanner = new Scanner(System.in);
 		int N = scanner.nextInt();
 		int[] arr = new int[N];
-		int min=1000000;
-		int max=-1000000;
+		int min=1000000; //범위를 정해주기
+		int max=-1000000; 
 		
 		for(int i=0; i<arr.length; i++ ) {
 			arr[i] = scanner.nextInt();
