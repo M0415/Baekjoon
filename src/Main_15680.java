26ba22382c8e0971a6f7fd9002d5aeb36d6e4364
minsu <danbikma@naver.com>
2022-03-17 21:03:59 +0900

    연세대학교

---
+++ src/Main_15680.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main_15680 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int N = scanner.nextInt();
+		
+		if(N==0) {
+			System.out.print("YONSEI");
+		} else if(N==1) {
+			System.out.print("Leading the Way to the Future");
+		}
+		
+		scanner.close();
+
+	}
+
+}
