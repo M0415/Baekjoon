bd970fe8390b2b13e96180831f4b33be31d47576
minsu <danbikma@naver.com>
2022-04-20 21:30:39 +0900

    new

---
+++ src/Main_16394.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main_16394 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		
+		int year = n - 1946;
+		System.out.print(year);
+		scanner.close();
+
+	}
+
+}
